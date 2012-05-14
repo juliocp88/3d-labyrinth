b6163047aba7e26cce4b9df659c111dc0fdb5a54
Hans.Strejc@gmail.com <Hans.Strejc@gmail.com@384cb064-f013-91bd-ddf6-59415a6643b1>
2012-05-14 12:52:03 +0000

    Comments

--- 3D-Labyrinth/src/mkz/labyrinth3D/game/Level.java
+++ 3D-Labyrinth/src/mkz/labyrinth3D/game/Level.java
@@ -9,21 +9,34 @@ import mkz.labyrinth3D.game.objects.ItemSet;
 import mkz.labyrinth3D.math.Vector3;
 
 /**
- *
+ * Contains level map, ball position and item set (gems).
  * @author Hans
  */
 public class Level
 {
+    /**Level map*/
     private Map map;
+    /**Gems*/
     private ItemSet itemSet;
+    /**Application context*/
     private Context context;
+    /**Ball starting position*/
     private Vector3 ballPosition;
     
+    /**
+     * Creates empty level.
+     * @param context application context
+     */
     public Level(Context context)
     {
         this.context = context;
     }
     
+    /**
+     * Loads and creates level and its resources
+     * @param ID    Level ID
+     * @param gl    OPENGL context
+     */
     public void load(int ID, GL11 gl)
     {
         Texture mapAtlasTex = new Texture(gl, context, R.drawable.map_atlas);
@@ -46,16 +59,28 @@ public class Level
         }
     }
 
+    /**
+     * Returns level map.
+     * @return level map
+     */
     public Map getMap()
     {
         return map;
     }
 
+    /**
+     * Returns item set.
+     * @return gems
+     */
     public ItemSet getItemSet()
     {
         return itemSet;
     }
 
+    /**
+     * Reurns ball starting position.
+     * @return ball starting position
+     */
     public Vector3 getBallPosition()
     {
         if (ballPosition == null)
